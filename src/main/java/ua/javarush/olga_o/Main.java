49ffe3796f21da3b73aa527dce3b8429ff5b0d8e
Olga O <zorina.olya@gmail.com>
2023-12-14 23:27:49 +0100

    Initialize project with mavent and add base MySQL queries

---
+++ src/main/java/ua/javarush/olga_o/Main.java
@@ -0,0 +1,94 @@
+package ua.javarush.olga_o;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.lettuce.core.RedisClient;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import ua.javarush.olga_o.dao.CityDAO;
+import ua.javarush.olga_o.dao.CountryDAO;
+import ua.javarush.olga_o.domain.City;
+import ua.javarush.olga_o.domain.Country;
+import ua.javarush.olga_o.domain.CountryLanguage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static java.util.Objects.nonNull;
+
+public class Main {
+    private final SessionFactory sessionFactory;
+    private final RedisClient redisClient;
+
+    private final ObjectMapper mapper;
+
+    private final CityDAO cityDAO;
+    private final CountryDAO countryDAO;
+
+    public Main() {
+        sessionFactory = prepareRelationalDb();
+        cityDAO = new CityDAO(sessionFactory);
+        countryDAO = new CountryDAO(sessionFactory);
+
+        redisClient = prepareRedisClient();
+        mapper = new ObjectMapper();
+    }
+
+    public static void main(String... param) {
+        Main main = new Main();
+        List<City> allCities = main.fetchData(main);
+        main.shutdown();
+    }
+
+    private SessionFactory prepareRelationalDb() {
+        final SessionFactory sessionFactory;
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3317/world");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        properties.put(Environment.HBM2DDL_AUTO, "validate");
+        properties.put(Environment.STATEMENT_BATCH_SIZE, "100");
+
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(City.class)
+                .addAnnotatedClass(Country.class)
+                .addAnnotatedClass(CountryLanguage.class)
+                .addProperties(properties)
+                .buildSessionFactory();
+        return sessionFactory;
+    }
+
+    private RedisClient prepareRedisClient() {
+        return null;
+    }
+
+    private void shutdown() {
+        if (nonNull(sessionFactory)) {
+            sessionFactory.close();
+        }
+        if (nonNull(redisClient)) {
+            redisClient.shutdown();
+        }
+    }
+
+    private List<City> fetchData(Main main) {
+        try (Session session = main.sessionFactory.getCurrentSession()) {
+            List<City> allCities = new ArrayList<>();
+            session.beginTransaction();
+            List<Country> countries = main.countryDAO.getAll();
+
+            int totalCount = main.cityDAO.getTotalCount();
+            int step = 500;
+            for (int i = 0; i < totalCount; i += step) {
+                allCities.addAll(main.cityDAO.getItems(i, step));
+            }
+            session.getTransaction().commit();
+            return allCities;
+        }
+    }
+}
---
+++ src/main/java/ua/javarush/olga_o/domain/Continent.java
@@ -0,0 +1,11 @@
+package ua.javarush.olga_o.domain;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
